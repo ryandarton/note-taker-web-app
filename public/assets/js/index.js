ee8b4a86abde262042f9e4dde83cb30d5944f95a
Ryan Darton <ryan@timemachinevideo.com>
2024-04-07 14:13:24 -0600

    Create server.js file and fixes referrence to index.js file in notes.html. Add console logs in index.js for testing.

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -32,25 +32,25 @@ const getNotes = () =>
   fetch('/api/notes', {
     method: 'GET',
     headers: {
-      'Content-Type': 'application/json'
-    }
+      'Content-Type': 'application/json',
+    },
   });
 
 const saveNote = (note) =>
   fetch('/api/notes', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify(note)
+    body: JSON.stringify(note),
   });
 
 const deleteNote = (id) =>
   fetch(`/api/notes/${id}`, {
     method: 'DELETE',
     headers: {
-      'Content-Type': 'application/json'
-    }
+      'Content-Type': 'application/json',
+    },
   });
 
 const renderActiveNote = () => {
@@ -73,9 +73,10 @@ const renderActiveNote = () => {
 };
 
 const handleNoteSave = () => {
+  console.log('handleNoteSave');
   const newNote = {
     title: noteTitle.value,
-    text: noteText.value
+    text: noteText.value,
   };
   saveNote(newNote).then(() => {
     getAndRenderNotes();
@@ -150,13 +151,7 @@ const renderNoteList = async (notes) => {
 
     if (delBtn) {
       const delBtnEl = document.createElement('i');
-      delBtnEl.classList.add(
-        'fas',
-        'fa-trash-alt',
-        'float-right',
-        'text-danger',
-        'delete-note'
-      );
+      delBtnEl.classList.add('fas', 'fa-trash-alt', 'float-right', 'text-danger', 'delete-note');
       delBtnEl.addEventListener('click', handleNoteDelete);
 
       liEl.append(delBtnEl);
@@ -184,7 +179,10 @@ const renderNoteList = async (notes) => {
 // Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
 
+console.log('window.location.pathname', window.location.pathname);
+
 if (window.location.pathname === '/notes') {
+  console.log('window.location.pathname === /notes');
   saveNoteBtn.addEventListener('click', handleNoteSave);
   newNoteBtn.addEventListener('click', handleNewNoteView);
   clearBtn.addEventListener('click', renderActiveNote);
---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Server error');
+      return;
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNote = { ...req.body, id: uuidv4() };
+
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Server error');
+      return;
+    }
+    const notes = JSON.parse(data);
+
+    notes.push(newNote);
+
+    fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+      if (err) {
+        res.status(500).send('Server error');
+        return;
+      }
+      res.json(newNote);
+    });
+  });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Server error');
+      return;
+    }
+    let notes = JSON.parse(data);
+    notes = notes.filter((note) => note.id !== noteId);
+
+    fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+      if (err) {
+        res.status(500).send('Server error');
+        return;
+      }
+      res.json({ message: 'Note deleted' });
+    });
+  });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
